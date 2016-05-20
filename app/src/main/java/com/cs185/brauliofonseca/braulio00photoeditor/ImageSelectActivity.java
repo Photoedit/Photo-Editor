1ae32162c098b9fbedb5c4aecb1e549c7eb07572
Braulio Fonseca <pop@Braulios-MacBook-Pro.local>
2016-05-20 02:43:00 -0700

    Gallery shows when selecting picture from image

--- app/src/main/java/com/cs185/brauliofonseca/braulio00photoeditor/ImageSelectActivity.java
+++ app/src/main/java/com/cs185/brauliofonseca/braulio00photoeditor/ImageSelectActivity.java
@@ -1,9 +1,15 @@
 package com.cs185.brauliofonseca.braulio00photoeditor;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 
@@ -12,6 +18,8 @@ public class ImageSelectActivity extends AppCompatActivity {
 
     private ImageButton image_button;
     private Toolbar bottomToolbar;
+    public static final int RESULT_GALLERY = 0;
+    public static final String DEBUG_GALLERY_CALLBACK = "DEBUG GALLERY CALLBACK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,12 @@ public class ImageSelectActivity extends AppCompatActivity {
         // Overwrite functions for the ImageButton view
         image_button = (ImageButton) findViewById(R.id.start_image_selection);
         image_button.setImageResource(R.drawable.default_screen);
+        image_button.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executePhotoGalleryIntent();
+            }
+        });
 
         // Overwrite functions for the Toolbar
         bottomToolbar = (Toolbar) findViewById(R.id.bottom_bar);
@@ -39,4 +53,47 @@ public class ImageSelectActivity extends AppCompatActivity {
 
         return true;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_GALLERY:
+                if (resultCode == RESULT_OK) {
+                    Uri selected_image_uri = data.getData();
+                    String image_path = getPathFromUri(selected_image_uri);
+
+
+                }
+        }
+
+
+        Log.d(DEBUG_GALLERY_CALLBACK, "Reached callback function");
+    }
+
+
+    // Function for bringing up the gallery intent
+    public void executePhotoGalleryIntent() {
+        Intent gallery_intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery_intent, RESULT_GALLERY);
+    }
+
+
+    // Helper function for retrieving path of image URI
+    public String getPathFromUri(Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+
+        String[] projection = { MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA );
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
+
 }
