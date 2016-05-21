1a3744f572a8293d4f3c796fc8f14b001de4ed97
Braulio Fonseca <pop@169-231-152-224.wireless.ucsb.edu>
2016-05-21 15:03:15 -0700

    Changed ImageButton to ImageView and image loads from gallery

--- app/src/main/java/com/cs185/brauliofonseca/braulio00photoeditor/ImageSelectActivity.java
+++ app/src/main/java/com/cs185/brauliofonseca/braulio00photoeditor/ImageSelectActivity.java
@@ -1,23 +1,30 @@
 package com.cs185.brauliofonseca.braulio00photoeditor;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 
 public class ImageSelectActivity extends AppCompatActivity {
 
-    private ImageButton image_button;
+    private ImageView image_button;
     private Toolbar bottomToolbar;
+    private Uri selected_image_uri;
     public static final int RESULT_GALLERY = 0;
     public static final String DEBUG_GALLERY_CALLBACK = "DEBUG GALLERY CALLBACK";
 
@@ -26,19 +33,30 @@ public class ImageSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_select);
 
+        // Setup read and write permission for app
+//        int write_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        if (write_permission != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+//        }
+//
+//        int read_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+//        if (read_permission != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+//        }
+
         // Top toolbar implementation
         Toolbar top_toolbar = (Toolbar) findViewById(R.id.top_bar);
         setSupportActionBar(top_toolbar);
 
         // Overwrite functions for the ImageButton view
-        image_button = (ImageButton) findViewById(R.id.start_image_selection);
+        image_button = (ImageView) findViewById(R.id.start_image);
         image_button.setImageResource(R.drawable.default_screen);
-        image_button.setOnClickListener(new ImageButton.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                executePhotoGalleryIntent();
-            }
-        });
+//        image_button.setOnClickListener(new ImageButton.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                executePhotoGalleryIntent();
+//            }
+//        });
 
         // Overwrite functions for the Toolbar
         bottomToolbar = (Toolbar) findViewById(R.id.bottom_bar);
@@ -55,21 +73,37 @@ public class ImageSelectActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.gallery_selection:
+                // On selection of gallery button
+                executePhotoGalleryIntent();
+                return true;
+            case R.id.revert_selection:
+                return true;
+            case R.id.save_selection:
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         switch (requestCode) {
             case RESULT_GALLERY:
                 if (resultCode == RESULT_OK) {
-                    Uri selected_image_uri = data.getData();
-                    String image_path = getPathFromUri(selected_image_uri);
-
+                    selected_image_uri = data.getData();
+                    image_button.setImageURI(selected_image_uri);
+                    Log.d(DEBUG_GALLERY_CALLBACK, "Reached inner conditional");
 
                 }
         }
 
-
-        Log.d(DEBUG_GALLERY_CALLBACK, "Reached callback function");
     }
 
 
